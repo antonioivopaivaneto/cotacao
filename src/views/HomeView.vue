556c6407148408b6b746cb0895261e273f30045b
junior <antonioivo.3@gmail.com>
2024-01-09 14:33:52 -0300

    layout ajustes

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,51 +1,65 @@
 <template>
   
-  <header class=" h-screen mb-12 overflow-hidden">
+  <header class="  mb-6 bg-slate-500 ">
     
-    <div class=" relative  text-center z-30 p-5 text-4xl text-white mt-32">
-      <h3 class="font-bold uppercase mb-20">Cotação Facilities </h3>
+    <div class=" relative  text-center z-30 p-2.5 text-4xl text-white ">
+      <h3 class="font-bold uppercase mb-10 mt-10">Cotação Facilities </h3>
     </div>
 
-    <div class="relative flex items-center justify-center">
+    <div class=" flex items-center justify-center ">
 
     
-    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2">
-
-      <div class="hover:bg-white hover:text-gray-700 font-bold cursor-pointer mx-2 relative text-center z-30 p-5 text-2xl text-white border-white border-2 bg-opacity-50 rounded-xl">
-      <h3>Cotar </h3>
-      <h3>Limpeza </h3>
+    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2 bg-red-400">
+      <div @mouseover="limpeza = limpezaB" @mouseleave="limpeza = limpezaA" class="hover:bg-white hover:text-gray-700 font-bold cursor-pointer mx-2 relative text-center z-30 p-2.5 text-2xl text-white border-white border-2 bg-opacity-50 rounded-xl">
+        <img :src="limpeza"  class="w-9 sm:w-24 mx-auto" alt="">
+        <h3 class="text-sm sm:text-lg ">Limpeza </h3>
     </div>
-    <div class="hover:bg-white hover:text-gray-700 font-bold cursor-pointer mx-2 relative text-center z-30 p-5 text-2xl text-white border-white border-2 bg-opacity-50 rounded-xl">
-      <h3>Cotar </h3>
-      <h3>Portaria </h3>
+    <div @mouseover="porteiro = porteiroB" @mouseleave="porteiro = porteiroA"  class="hover:bg-white hover:text-gray-700 font-bold cursor-pointer mx-2 relative text-center z-30 p-2.5 text-2xl text-white border-white border-2 bg-opacity-50 rounded-xl">
+      <img :src="porteiro"  class="w-9 sm:w-24 mx-auto" alt="">
+      <h3 class="text-sm sm:text-lg ">Zeladoria </h3>
     </div>
-    <div class="hover:bg-white hover:text-gray-700 font-bold cursor-pointer mx-2 relative text-center z-30 p-5 text-2xl text-white border-white border-2 bg-opacity-50 rounded-xl">
-      <h3>Cotar </h3>
-      <h3>Controle de Acesso </h3>
+    <div @mouseover="acesso = acessoB" @mouseleave="acesso = acessoA" class="hover:bg-white hover:text-gray-700 font-bold cursor-pointer mx-2 relative text-center z-30 p-2.5 text-2xl text-white border-white border-2 bg-opacity-50 rounded-xl">
+      <img :src="acesso"  class="w-9 sm:w-24 mx-auto" alt="">
+      <h3 class="text-sm sm:text-lg ">Controle de Acesso </h3>
     </div>
-    <div class="hover:bg-white hover:text-gray-700 font-bold cursor-pointer mx-2 relative text-center z-30 p-5 text-2xl text-white border-white border-2 bg-opacity-50 rounded-xl">
-      <h3>Cotar </h3>
-      <h3>Jardinagem </h3>
+    <div @mouseover="jardin = jardinB" @mouseleave="jardin = jardinA" class="hover:bg-white hover:text-gray-700 font-bold cursor-pointer mx-2 relative text-center z-30 p-2.5 text-2xl text-white border-white border-2 bg-opacity-50 rounded-xl">
+      <img :src="jardin"  class="w-9 sm:w-24 mx-auto" alt="">
+      <h3 class="text-sm sm:text-lg ">Jardinagem </h3>
     </div>
-    <div class="hover:bg-white hover:text-gray-700 font-bold cursor-pointer mx-2 relative text-center z-30 p-5 text-2xl text-white border-white border-2 bg-opacity-50 rounded-xl">
-      <h3>Cotar </h3>
-      <h3>Manutencista</h3>
+    <div @mouseover="manu = manuA" @mouseleave="manu = manuB" class="hover:bg-white hover:text-gray-700 font-bold cursor-pointer mx-2 relative text-center z-30 p-2.5 text-2xl text-white border-white border-2 bg-opacity-50 rounded-xl">
+      <img :src="manu"  class="w-9 sm:w-24 mx-auto" alt="">
+      <h3 class="text-sm sm:text-lg ">Manutencista</h3>
+    </div>
+    <div @mouseover="cope = copeA" @mouseleave="cope = copeB" class="hover:bg-white hover:text-gray-700 font-bold cursor-pointer mx-2 relative text-center z-30 p-2.5 text-2xl text-white border-white border-2 bg-opacity-50 rounded-xl">
+      <img :src="cope"  class="w-9 sm:w-24 mx-auto" alt="">
+      <h3 class="text-sm sm:text-lg ">Copeira</h3>
+    </div>
+    <div @mouseover="recep = recepA" @mouseleave="recep = recepB" class="hover:bg-white hover:text-gray-700 font-bold cursor-pointer mx-2 relative text-center z-30 p-2.5 text-2xl text-white border-white border-2 bg-opacity-50 rounded-xl">
+      <img :src="recep"  class="w-9 sm:w-24 mx-auto" alt="">
+      <h3 class="text-sm sm:text-lg ">Atendimento e Recepção</h3>
+    </div>
+    <div @mouseover="custo = custoA" @mouseleave="custo = custoB" class="hover:bg-white hover:text-gray-700 font-bold cursor-pointer mx-2 relative text-center z-30 p-2.5  text-2xl text-white border-white border-2 bg-opacity-50 rounded-xl">
+      <img :src="custo"  class="w-9 sm:w-24 mx-auto" alt="">
+      <h3 class="text-sm sm:text-lg ">Outros</h3>
     </div>
     </div>
   </div>
    
     
-    <div class="absolute inset-0 z-0">
+    <div class="absolute inset-0 z-0 h-screen">
         <video autoplay loop muted class="w-full h-full object-cover  ">
           <source :src="video" type="video/mp4">
           Seu navegador não suporta a tag de vídeo.
         </video>
       </div>
 
-      <div class="w-48 float-end mt-64 bg-white hover:text-gray-700 font-bold cursor-pointer mx-2 relative text-center z-30 p-5 text-2xl text-white border-white border-2  rounded-s-xl">
+      <a target="_blank" href="https://safetyfacilities.com.br/"><div class="w-24 float-end mr-0  bg-white hover:text-gray-700 font-bold cursor-pointer mx-2 relative text-center z-30 p-2.5 text-2xl text-white border-white border-2  rounded-s-xl">
         <img :src="logo" class="w-48  mx-auto mb-4" alt="">
+        
 
     </div>
+
+  </a>
 
       <!-- Sobreposição escura -->
       <div class="absolute inset-0 bg-black opacity-50 z-10"></div>
@@ -55,8 +69,39 @@
 </template>
 
 <script setup>
-import video from "./../../public/video/wallpaper.mp4"
+import video from "./../assets/video/wallpaper.mp4"
 import logo from "./../assets/logo.png"
 
+import limpezaA from "./../assets/icones/limpeza (2).png"
+import limpezaB from "./../assets/icones/limpeza (1).png"
+import porteiroA from "./../assets/icones/porteiro (1).png"
+import porteiroB from "./../assets/icones/porteiro (2).png"
+import acessoA from "./../assets/icones/controle-de-acesso (1).png"
+import acessoB from "./../assets/icones/controle-de-acesso.png"
+import jardinA from "./../assets/icones/jardineiro (1).png"
+import jardinB from "./../assets/icones/jardineiro.png"
+import manuA from "./../assets/icones/manutencao (1).png"
+import manuB from "./../assets/icones/manutencao.png"
+import copeA from "./../assets/icones/garcom (1).png"
+import copeB from "./../assets/icones/garcom.png"
+import recepA from "./../assets/icones/recepcao (1).png"
+import recepB from "./../assets/icones/recepcao.png"
+import custoA from "./../assets/icones/customizar (1).png"
+import custoB from "./../assets/icones/customizar.png"
+
+import { ref } from "vue"
+
+
+const limpeza = ref(limpezaA)
+const porteiro = ref(porteiroA)
+const acesso = ref(acessoA)
+const jardin = ref(jardinA)
+const manu = ref(manuB)
+const cope = ref(copeB)
+const recep = ref(recepB)
+const custo = ref(custoB)
 
 </script>
+
+
+<style ></style>
